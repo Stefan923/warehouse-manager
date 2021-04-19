27ae17404a0be574be9b92a657cb456083840e15
Stefan923 <stefanpopescu923@gmail.com>
2021-04-19 20:09:02 +0300

    Added setId method

--- src/main/java/me/stefan923/ordermanager/model/Client.java
+++ src/main/java/me/stefan923/ordermanager/model/Client.java
@@ -2,11 +2,13 @@ package me.stefan923.ordermanager.model;
 
 public class Client {
 
-    private final int id;
+    private int id;
     private String name;
     private String address;
     private String email;
     private int age;
+
+    public Client() { }
 
     public Client(int id, String name, String address, String email, int age) {
         this.id = id;
@@ -18,6 +20,10 @@ public class Client {
 
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
